07374ff489974c30ed675cd286d434b0011c8b7c
ask0908 <askdfjaslk@naver.com>
2022-04-20 13:09:51 +0900

    6장 끝까지 구현. 공부 내용 노션에 정리

---
+++ components/FeedList.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {FlatList, StyleSheet, View} from 'react-native';
+import FeedListItem from './FeedListItem';
+
+function FeedList({logs, onScrolledToBottom}) {
+    const onScroll = e => {
+        // 함수가 없으면 아무것도 하지 않게 하는 예외처리
+        if (!onScrolledToBottom) {
+            return;
+        }
+        const {contentSize, layoutMeasurement, contentOffset} = e.nativeEvent;
+        const distanceFromBottom =
+            contentSize.height - layoutMeasurement.height - contentOffset.y;
+
+        // 아이폰에서 계속 스크롤하면 FlatList의 바깥 부분이 스크롤되는 현상에 대한 예외처리
+        // 스크롤되는 걸 막는 게 아닌 contentSize.height가 더 클 때만 작동하게 하는 것이다
+        if (
+            contentSize.height > layoutMeasurement.height &&
+            distanceFromBottom < 72
+        ) {
+            onScrolledToBottom(true);
+        } else {
+            onScrolledToBottom(false);
+        }
+    };
+    return (
+        <FlatList
+            data={logs}
+            style={styles.block}
+            renderItem={({item}) => <FeedListItem log={item} />}
+            keyExtractor={log => log.id}
+            ItemSeparatorComponent={() => <View style={styles.separator} />}
+            onScroll={onScroll}
+        />
+    );
+}
+
+const styles = StyleSheet.create({
+    block: {
+        flex: 1,
+    },
+    separator: {
+        backgroundColor: '#E0E0E0',
+        height: 1,
+        width: '100%',
+    },
+});
+
+export default FeedList;
--- screens/FeedsScreen.js
+++ screens/FeedsScreen.js
@@ -1,13 +1,21 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {View, StyleSheet} from 'react-native';
+import FeedList from '../components/FeedList';
 import FloatingWriteButton from '../components/FloatingWriteButton';
 import LogContext from '../contexts/LogContext';
 
 function FeedsScreen() {
     const {logs} = useContext(LogContext);
+    const [hidden, setHidden] = useState(false);
+    const onScrolledToBottom = isBottom => {
+        if (hidden !== isBottom) {
+            setHidden(isBottom);
+        }
+    };
     return (
         <View style={styles.block}>
-            <FloatingWriteButton />
+            <FeedList logs={logs} onScrolledToBottom={onScrolledToBottom} />
+            <FloatingWriteButton hidden={hidden} />
         </View>
     );
 }
